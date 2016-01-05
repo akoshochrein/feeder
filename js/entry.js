8577fab877d229abd2a2bcbb1255acfc158e88f3
akoskaaa <akos.hochrein@prezi.com>
2016-01-05 10:04:06 +0100

    hot reload with react. god.

---
+++ js/entry.js
@@ -0,0 +1,12 @@
+require('../less/main.less');
+
+'use strict';
+
+import React from "react";
+
+React.render(
+    <div>
+        Hello World!
+    </div>,
+    document.getElementById('content')
+);
