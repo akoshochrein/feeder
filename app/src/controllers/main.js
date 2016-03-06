2e0fa03634785d546f1f225dcdcd2b4c8d43b94a
akoskaaa <akos.hochrein@prezi.com>
2016-03-06 19:04:48 +0100

    use scheduler for reloading feeds

--- app/src/controllers/main.js
+++ app/src/controllers/main.js
@@ -4,18 +4,31 @@ var $ = require('jquery'),
     RSSListManagerFactory = require('../components/RSSListManager.jsx'),
     bacon = require('baconjs');
 
-
 var RSSController = (function () {
     var state = {},
-        factory = RSSListManagerFactory;
+        factory = RSSListManagerFactory,
+        scheduler = bacon.interval(1000, true);
 
     function init () {
+        scheduler.flatMap(getAllFeeds).onValue(changeState);
         // TODO init state from localstorage
-
-
         changeState({
             'rssItems': [] // TODO maybe refactor to an object with hashes of urls
         });
+    }
+
+    function getAllFeeds (_) {
+        // [S<responses>] -> S<[responses]>
+        var urls = state.rssItems.map(function (i) {return i.url});
+        var promises = urls.map(getPromiseForUrl)
+        var zippedPromises = bacon.zipAsArray(promises);
+
+        return zippedPromises.map(mapResponsesToRSSItems);
+    }
+
+    function getPromiseForUrl (url) {
+        // returns 1 promise from 1 ajax call for 1 url
+        return bacon.fromPromise($.ajax({url: url}));
     }
 
     function changeState (newState) {
@@ -49,29 +62,11 @@ var RSSController = (function () {
 
     function createRSSFromUrl (url) {
         var responseStream = bacon.fromPromise($.ajax({
-            method: 'get',
             url: url
         }));
 
         // xml2js alternatives that are not async
-        var newRssItemStream = responseStream.map(function (response) {
-            var result = xml2json.parser(new XMLSerializer().serializeToString(response)),
-                rssChannel = result.rss.channel,
-                rssTitle = rssChannel.title,
-                rssUrl = rssChannel.link,
-                rssItems = rssChannel.item;
-
-            return {
-                'title': rssTitle,
-                'url': rssUrl,
-                'feedItems': rssItems.map(function (item) {
-                    return {
-                        'title': item.title,
-                        'url': item.link
-                    }
-                })
-            }
-        });
+        var newRssItemStream = responseStream.map(mapResponseToRSSItem);
 
         newRssItemStream.onValue(function (newRssItem) {
             var currentRSSItems = state.rssItems;
@@ -80,6 +75,36 @@ var RSSController = (function () {
                 rssItems: currentRSSItems
             });
         });
+    }
+
+    function mapResponsesToRSSItems (responses) {
+        return {
+            rssItems: responses.map(mapResponseToRSSItem)
+        };
+    }
+
+    function mapResponseToRSSItem (response) {
+        var responseString = typeof(response) === 'string'
+            ? response
+            : new XMLSerializer().serializeToString(response),
+            result = xml2json.parser(responseString),
+            rssChannel = result.rss.channel,
+            rssTitle = rssChannel.title,
+            rssUrl = rssChannel['atom%3alink'].href,
+            webUrl = rssChannel.link,
+            rssItems = rssChannel.item;
+
+        return {
+            'title': rssTitle,
+            'url': rssUrl,
+            'webUrl': webUrl,
+            'feedItems': rssItems.map(function (item) {
+                return {
+                    'title': item.title,
+                    'url': item.link
+                }
+            })
+        }
     }
 
     return {
